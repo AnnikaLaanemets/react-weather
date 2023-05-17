a4ca322d67e03adf8be1156e2db91b9eadafbf18
AnnikaLaa <annika0096@gmail.com>
2023-05-17 22:02:01 +0300

    axios+api

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <div className="container">
         <div className="grid wide">
           <div>
-            <WeatherToday />
+            <WeatherToday defaultCity="Barcelona" />
           </div>
           <div>
             <Sunset />
@@ -21,7 +21,10 @@ function App() {
       </div>
       <footer>
         {" "}
-        <a href="https://github.com/AnnikaLaanemets/react-weather-board">
+        <a
+          href="https://github.com/AnnikaLaanemets/react-weather-board"
+          target="_blank"
+        >
           https://github.com/AnnikaLaanemets/react-weather-board
         </a>
       </footer>
--- src/WeatherToday.js
+++ src/WeatherToday.js
@@ -1,66 +1,94 @@
+import React, { useState } from "react";
+import axios from "axios";
 import "./App.css";
 
-export default function WeatherToday() {
-  return (
-    <div className="grid two-rows">
-      <div className="search-form">
-        <form>
-          <input
-            type="text"
-            placeholder="Search city"
-            autoComplete="off"
-            autoFocus="on"
-            id="search-input"
-          />
-          <input type="submit" value="Submit" />
-        </form>
-        <button>Show my current location</button>
-      </div>
+export default function WeatherToday(props) {
+  let [data, setData] = useState({ ready: false });
 
-      <div className="grid two-columns">
-        <div className="description">
-          <h3>Partly sunny</h3>
-          <div>
-            <span>Precipitation: </span>
-            <span>12%</span>
-          </div>
-          <div>
-            <span>Wind: </span>
-            <span>8 km/h</span>
-          </div>
-          <div>
-            <span>Humidity: </span>
-            <span>82%</span>
-          </div>
-          <div>
-            <span>Visibility: </span>
-            <span id="visibility">10 km</span>
-          </div>
-          <div>
-            <span>UV-index: </span>
-            <span>3</span>
-          </div>
-        </div>
+  function handleresponse(response) {
+    console.log(response);
+    setData({
+      ready: true,
+      city: response.data.city,
+      country: response.data.country,
+      temperature: response.data.temperature.current,
+      feels: response.data.temperature.feels_like,
+      description: response.data.description,
+      humidity: response.data.temperature.humidity,
+      wind: response.data.wind.speed,
+    });
+  }
 
-        <div className="main">
-          <h1>Barcelona, Spain</h1>
-          <div id="date">Sunday 14:00</div>
-          <div className="grid wide">
-            <span id="temperature">21°C</span>
-            <img
-              className="icon"
-              src={require("./icons/rain.png")}
-              width="200px"
-              alt="weather icon"
+  if (data.ready) {
+    return (
+      <div className="grid two-rows">
+        <div className="search-form">
+          <form>
+            <input
+              type="text"
+              placeholder="Search city"
+              autoComplete="off"
+              autoFocus="on"
+              id="search-input"
             />
+            <input type="submit" value="Submit" />
+          </form>
+          <button>Show my current location</button>
+        </div>
+
+        <div className="grid two-columns">
+          <div className="description">
+            <h3>{data.description}</h3>
+            <div>
+              <span>Precipitation: </span>
+              <span>12%</span>
+            </div>
+            <div>
+              <span>Wind: </span>
+              <span>{data.wind}</span>
+            </div>
+            <div>
+              <span>Humidity: </span>
+              <span>{data.humidity}</span>
+            </div>
+            <div>
+              <span>Visibility: </span>
+              <span id="visibility">10 km</span>
+            </div>
+            <div>
+              <span>UV-index: </span>
+              <span>3</span>
+            </div>
           </div>
-          <br />
-          <div>
-            <span>Feels like: </span>
-            <span>22°C</span>
+
+          <div className="main">
+            <h1>
+              {data.city}, {data.country}
+            </h1>
+            <div id="date">Sunday 14:00</div>
+            <div className="grid wide">
+              <span id="temperature">{Math.round(data.temperature)}</span>
+              <img
+                className="icon"
+                src={require("./icons/rain.png")}
+                width="200px"
+                alt="weather icon"
+              />
+            </div>
+            <br />
+            <div>
+              <span>Feels like: {Math.round(data.feels)} </span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const key = "baf15f814713odta8a4baa99ed0733e5";
+    let query = "Copenhagen";
+    let url = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${key}&units=metric`;
+    axios.get(url).then(handleresponse);
+
+    return "Loading...";
+  }
 }
